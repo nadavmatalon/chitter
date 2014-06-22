18f479eed65f8cf4a78e5cbf074a9fd3a534c2ba
Nadav <nadav.matalon@gmail.com>
2014-06-22 14:07:51 +0100

    fixed time stamp for peeps

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,7 +5,7 @@ class Peep
 	include DataMapper::Resource
 
 	property :id, Serial
-	property :time, String, :default => Proc.new {|r, p| "[" + Time.now.strftime("%C %B %Y %I:%M%p") + "]"}
+	property :time, String, :default => Proc.new {|r, p| "[#{Date.today.day}-#{Date.today.month}-#{Date.today.year} #{Time.now.strftime("%I:%M%p")}]"}
 	property :content, Text, required: true, message: "A peep must have content"
 
 
