75465b9b4dba953075dbf593007755b13a961df9
Nadav <nadav.matalon@gmail.com>
2014-08-23 01:14:09 +0100

    commented out jquery testing function

--- public/javascript/jquery-functions.js
+++ public/javascript/jquery-functions.js
@@ -1,15 +1,15 @@
 
-$.ajaxSetup ({  
- cache: false  
-});
+// $.ajaxSetup ({  
+//  cache: false  
+// });
 
-$(document).ready(function() { 
-	update_time();
-});
+// $(document).ready(function() { 
+// 	update_time();
+// });
 
-function update_time() {
-	$.get("/update", function(data) {
-		$("#update-div").html(data);
-		window.setTimeout(update_time, 2000);
-	}, 'text');
-}
+// function update_time() {
+// 	$.get("/update", function(data) {
+// 		$("#update-div").html(data);
+// 		window.setTimeout(update_time, 2000);
+// 	}, 'text');
+// }
