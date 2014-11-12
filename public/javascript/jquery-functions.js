fd4d7d191a6ccef63172fe30667c90eecdb67c23
Nadav <nadav.matalon@gmail.com>
2014-11-12 16:53:37 +0000

    code refactoring of js files

--- public/javascript/jquery-functions.js
+++ public/javascript/jquery-functions.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-	$('#sign-in-window').click(function() {		
+	$('#sign-in-window').click(function() {
 		var signInBox = $('.sign-in-popup');
 		$(signInBox).fadeIn(200);
-		var marginTop = ($(signInBox).height() + 24) / 2; 
-		var marginLeft = ($(signInBox).width() + 24) / 2; 	
+		var marginTop = ($(signInBox).height() + 24) / 2;
+		var marginLeft = ($(signInBox).width() + 24) / 2;
 		$(signInBox).css({ 
 			'margin-top': -marginTop,
 			'margin-left': -marginLeft
-		});		
-		$('#mask').removeClass('hiding');		
-		$('#mask').addClass('showing');		
+		});
+		$('#mask').removeClass('hiding');
+		$('#mask').addClass('showing');
 		return false;
 	});
 
-	$('#close-button, #mask').on('click', function() { 
+	$('#close-button, #mask').on('click', function() {
 		$('.sign-in-popup').fadeOut(200);
 		$('#mask').removeClass('showing'); 
 		$('#mask').addClass('hiding'); 
